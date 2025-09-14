7ead9926ebb96041f9531008b6873d65f3950392
KhalisaGhaizani <khalisasila@gmail.com>
2025-09-14 21:56:56 +0700

    db

--- db/db.go
+++ db/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func New() (*sql.DB, error) {
-	databaseUrl := "postgresql://postgres.akzxyqjulxohiwoejhny:9n6jfhPachFY4Bhg@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres"
+	databaseUrl := "postgresql://postgres.akzxyqjulxohiwoejhny:271g3YaNmsvWxSqB@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require"
 	if databaseUrl == "" {
 		return nil, fmt.Errorf("POSTGRESQL_URL environment variable not set")
 	}

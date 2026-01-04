3634f6950bb2dfaaa020c7d6a11c09ad9fd5ed08
Yanzz231 <mrpartys05@gmail.com>
2026-01-05 03:47:55 +0700

    fix: welcome text

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Samsung Voice Backend API",
+			"message": "VoiceChess Backend API",
 			"version": "1.0.0",
 			"status":  "running",
 		})

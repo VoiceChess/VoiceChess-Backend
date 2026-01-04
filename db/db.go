8f8d243cf5ed4c045df1b4a85a47d95c48bbdefe
Yanzz231 <mrpartys05@gmail.com>
2026-01-05 02:20:22 +0700

    fix: database url sql

--- db/db.go
+++ db/db.go
@@ -3,12 +3,13 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func New() (*sql.DB, error) {
-	databaseUrl := "postgresql://postgres.akzxyqjulxohiwoejhny:271g3YaNmsvWxSqB@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require"
+	databaseUrl := os.Getenv("POSTGRESQL_URL")
 	//databaseUrl := "postgres://postgres:@localhost:5432/chessmate?sslmode=disable"
 	if databaseUrl == "" {
 		return nil, fmt.Errorf("POSTGRESQL_URL environment variable not set")
